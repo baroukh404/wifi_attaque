17bff7bb58c19ea489d458b3ee6e3be84609fb2f
baroukh404 <rrabeandriamarozoky@gmail.com>
2022-06-06 20:53:23 +0300

    file deleted

--- app.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system("netsh wlan show profiles Smartone_Direction key=clear > output.txt")
-with open("output.txt", "r") as fichier:
-    fichier = fichier.read()
-    print(fichier)
